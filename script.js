c7355a76bbb8f38b515e32fd82ff85d189b2799e
Andy Nguyen <nguyen.andy67@gmail.com>
2020-03-07 22:23:37 +1030

    added prompt to ask for case numbers and special characters

--- script.js
+++ script.js
@@ -19,7 +19,20 @@ function UserInputs(){
   while (passwordLength < 8 || passwordLength > 128){
     var passwordLength = prompt("Enter a number between 8 and 128");
     }  
-    console.log(passwordLength);
+  
+    
+  var uppercase = confirm("would you like uppercase");
+  var lowercase = confirm("would you like lowercase");
+  var numbers = confirm("would you like numbers");
+  var specialCharacters = confirm("would you like special charaters");
+  
+  while(uppercase == false && lowercase == false && numbers == false && specialCharacters == false){
+    alert("please pick one");
+    var uppercase = confirm("would you like uppercase");
+    var lowercase = confirm("would you like lowercase");
+    var numbers = confirm("would you like numbers");
+    var specialCharacters = confirm("would you like special charaters");
+  }
 }
 
 
