2866f22fcae284bcd6c11f77ab87c8caf3559796
Andy Nguyen <nguyen.andy67@gmail.com>
2020-03-07 23:24:41 +1030

    updated inputs to work correctly

--- script.js
+++ script.js
@@ -13,14 +13,13 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+//user input function 
 function UserInputs(){
   var passwordLength = 0
   while (passwordLength < 8 || passwordLength > 128){
     var passwordLength = prompt("Enter a number between 8 and 128");
-    }  
-  
-    
+    } 
+
   var uppercase = confirm("would you like uppercase");
   var lowercase = confirm("would you like lowercase");
   var numbers = confirm("would you like numbers");
@@ -32,10 +31,31 @@ function UserInputs(){
     var lowercase = confirm("would you like lowercase");
     var numbers = confirm("would you like numbers");
     var specialCharacters = confirm("would you like special charaters");
+    
   }
+  console.log(uppercase)
+  var selectionCounter = 0
+  if (uppercase== true){
+    selectionCounter++;
+  }
+  if (lowercase== true){
+    selectionCounter++;
+  }
+  if (numbers== true){
+    selectionCounter++;
+  }
+  if (specialCharacters== true){
+    selectionCounter++;
+  }
+  console.log(selectionCounter);
+
 }
 
 
+
+
+
+
 // Add event listener to generate buttons
-// getting inputs from the user
+
 generateBtn.addEventListener("click", UserInputs);
